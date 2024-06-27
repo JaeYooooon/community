d8dd7dba76c3286d7c75730af71cc70205a0c0d4
dhy04 <dhy04050@naver.com>
2024-06-27 21:25:56 +0900

    fix: Post 엔티티 @NoArgsConstructor 추가

--- src/main/java/com/jy/community/domain/post/Post.java
+++ src/main/java/com/jy/community/domain/post/Post.java
@@ -12,11 +12,13 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "posts")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Post {
 
