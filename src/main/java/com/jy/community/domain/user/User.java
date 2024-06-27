9ad36c4e2244f8c46b7ac82b6c1244df995745d5
dhy04 <dhy04050@naver.com>
2024-06-27 14:37:02 +0900

    fix: User 엔티티 수정 1. @Builder 어노테이션 추가 2. 권한 추가

--- src/main/java/com/jy/community/domain/user/User.java
+++ src/main/java/com/jy/community/domain/user/User.java
@@ -1,23 +1,33 @@
 package com.jy.community.domain.user;
 
+import com.jy.community.domain.user.type.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String username;
   private String password;
   private String nickname;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
 }
---
+++ src/main/java/com/jy/community/domain/user/type/Role.java
@@ -0,0 +1,11 @@
+package com.jy.community.domain.user.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+  USER,
+  ADMIN
+}
